7f879e9b5505f3e07057cb139fa83bbb88ae1448
Matheus Leodonel <mleodonel@gmail.com>
2021-12-09 12:07:26 -0300

    add findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,27 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		System.out.println("------- FindById Test -------");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+
+		System.out.println("\n------- FindByDepartment Test -------");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
