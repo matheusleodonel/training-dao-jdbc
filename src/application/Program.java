4a5a7bb22134a8fcee9e2704f123fa496a52ff5b
Matheus Leodonel <mleodonel@gmail.com>
2021-12-09 12:15:28 -0300

    Add findAll method

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,11 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n------- FindAll Test -------");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
