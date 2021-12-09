11e6eb86121f0aae6e9037acd6283612f5fb63a6
Matheus Leodonel <mleodonel@gmail.com>
2021-12-09 12:31:16 -0300

    Add insert seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,5 +30,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n------- Seller insert Test -------");
+		Seller addSeller = new Seller(null, "Matheus", "matheus@gmail.com", new Date(), 6000.0, department);
+		sellerDao.insert(addSeller);
+		System.out.println("New seller successfully inserted! Your id: " + addSeller.getId());
 	}
 }
